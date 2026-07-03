ae47bce74f4b5d53799ffe2fc42155d35ec1a3bf
Asifur <asifurr38@gmail.com>
2026-07-04 02:07:54 +0600

    Task 3

---
+++ AWT_Lab Task 03/university-system-api/src/enrollment/enrollment.service.ts
@@ -0,0 +1,48 @@
+import {
+  Injectable,
+  Inject,
+  forwardRef,
+} from '@nestjs/common';
+
+import { CourseService } from '../course/course.service';
+import { NotificationService } from '../notification/notification.service';
+
+@Injectable()
+export class EnrollmentService {
+
+  constructor(
+    private readonly courseService: CourseService,
+
+    @Inject(forwardRef(() => NotificationService))
+    private readonly notificationService: NotificationService,
+  ) {}
+
+  getEnrollments() {
+    return {
+      message: 'All enrollments fetched',
+      data: [],
+    };
+  }
+
+  enrollStudent(
+    studentName: string,
+    courseId: string,
+  ) {
+
+    const course =
+      this.courseService.getCourseById(courseId);
+
+    const notification =
+      this.notificationService.sendNotification(
+        studentName,
+        'Welcome!',
+      );
+
+    return {
+      message: 'Student enrolled successfully',
+      student: studentName,
+      course,
+      notification,
+    };
+  }
+}
